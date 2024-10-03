ff6b657837e852266201ae8658a5b98ccf624868
Andrey Burov <anburov@mons.ru>
2024-10-04 00:07:58 +0300

    deploy

--- src/app/MainRoutes.tsx
+++ src/app/MainRoutes.tsx
@@ -2,32 +2,23 @@
 import { useLayoutEffect } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import { userStore } from '../entities/user/store'
+import AuthPage from '../pages/AuthPage/AuthPage'
 import MessengerPage from '../pages/MessengerPage/MessengerPage'
 import PrivateRouter from '../shared/HOC/PrivateRouter/PrivateRouter'
-import AuthPage from '../pages/AuthPage/AuthPage'
 
 export default function MainRoutes() {
 	const navigate = useNavigate()
 
 	useLayoutEffect(() => {
 		const handleMessage = event => {
-			// if (event.origin !== 'http://localhost:5173') {
-			// 	return
-			// }
 			const { set: setUser } = userStore
-			console.log('Salam:', event)
-			console.log('Salam2:', event.data)
+
 			const { user, message } = event.data
-			console.log('🚀 ~ handleMessage ~ event:', event)
+
 			if (message === 'init') {
 				setUser(user)
 				navigate('/')
 			}
-
-			const responseMessage = { user, status: 'success' }
-			window.parent.postMessage(responseMessage, '*')
-
-			console.log('Полученные данные пользователя:', user)
 		}
 
 		window.addEventListener('message', handleMessage)
